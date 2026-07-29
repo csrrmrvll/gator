9b7b8aa875a252cf4b92d96923da7ac3eb158043
César Romero Avello <cesar.romero.avello@ing.com>
2026-07-29 12:29:00 +0200

    feat(commands): improved following bootdotdev solution files

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/csrrmrvll/gator/internal/command"
 	"github.com/csrrmrvll/gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
 
-	state := command.State{Config: &cfg}
-	commands := command.Commands{}
-	commands.Register("login", command.HandlerLogin)
-
-	args := os.Args[1:]
-	if len(args) < 1 {
-		log.Fatalf("no command provided")
+	programState := &state{
+		cfg: &cfg,
 	}
 
-	cmd := command.Command{Name: args[0], Args: args[1:]}
-	err = commands.Run(&state, cmd)
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
-		log.Fatalf("error running command: %v", err)
+		log.Fatal(err)
 	}
 }
