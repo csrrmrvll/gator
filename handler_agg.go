e45852aaf0078e63dbcf7ec1f489fcb43ae680df
César Romero Avello <cesar.romero.avello@ing.com>
2026-07-31 09:37:21 +0200

    feat(rss): improved following bootdotdev solution files

--- handler_agg.go
+++ handler_agg.go
@@ -2,70 +2,14 @@ package main
 
 import (
 	"context"
-	"encoding/xml"
 	"fmt"
-	"html"
-	"io"
-	"net/http"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	// Call the database function to get the aggregated data
 	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
 	if err != nil {
-		return fmt.Errorf("couldn't retrieve aggregated data: %w", err)
+		return fmt.Errorf("couldn't fetch feed: %w", err)
 	}
-
-	// Print the data
-	fmt.Println(feed)
+	fmt.Printf("Feed: %+v\n", feed)
 	return nil
-}
-
-type RSSFeed struct {
-	Channel struct {
-		Title       string    `xml:"title"`
-		Link        string    `xml:"link"`
-		Description string    `xml:"description"`
-		Item        []RSSItem `xml:"item"`
-	} `xml:"channel"`
-}
-
-type RSSItem struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	Description string `xml:"description"`
-	PubDate     string `xml:"pubDate"`
-}
-
-func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
-	// Fetch the RSS feed from the provided URL
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, feedURL, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %w", err)
-	}
-	req.Header.Set("User-Agent", "gator")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch feed: %w", err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to fetch feed: status code %d", resp.StatusCode)
-	}
-	defer resp.Body.Close()
-
-	// Parse the RSS feed
-	var feed RSSFeed
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read feed: %w", err)
-	}
-	if err := xml.Unmarshal(body, &feed); err != nil {
-		return nil, fmt.Errorf("failed to parse feed: %w", err)
-	}
-
-	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
-	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
-	return &feed, nil
 }
